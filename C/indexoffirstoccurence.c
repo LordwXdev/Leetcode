c2209405df6799402be6aaaec67fbec1e4765021
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-04-08 23:59:27 +0800

    ..

---
+++ C/indexoffirstoccurence.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int strStr(char* haystack, char* needle) {
+    int hLen = strlen(haystack);
+    int nLen = strlen(needle);
+
+    for (int i = 0; i <= hLen - nLen; i++) {
+        if (strncmp(haystack + i, needle, nLen) == 0)
+            return i;
+    }
+    return -1;
+}
