5cea0f0318f92c30b7eb94061a2d389e2028e8db
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-04-08 23:58:27 +0800

    find the index of the first occurence in a string

---
+++ C++/indexofforstoccurence.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        size_t pos = haystack.find(needle);
+        return (pos == string::npos) ? -1 : (int)pos;
+    }
+};
