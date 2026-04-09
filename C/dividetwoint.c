0e770064f84d33fd6da0ebd9efb0737285a07a5d
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-04-09 23:41:29 +0800

    divide two integers using c

---
+++ C/dividetwoint.c
@@ -0,0 +1,28 @@
+#include <limits.h>
+#include <stdlib.h>
+
+int divide(int dividend, int divisor) {
+    if (dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+
+    int negative = (dividend < 0) ^ (divisor < 0);
+
+    long dvd = labs(dividend);
+    long dvs = labs(divisor);
+    int result = 0;
+
+    while (dvd >= dvs) {
+        long temp = dvs;
+        int multiple = 1;
+
+        while (dvd >= (temp << 1)) {
+            temp <<= 1;
+            multiple <<= 1;
+        }
+
+        dvd -= temp;
+        result += multiple;
+    }
+
+    return negative ? -result : result;
+}
