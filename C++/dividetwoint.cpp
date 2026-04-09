7a294b9a27f61545403a773aa7d3d4fbb401325a
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-04-09 23:44:48 +0800

    ..

---
+++ C++/dividetwoint.cpp
@@ -0,0 +1,31 @@
+#include <climits>
+#include <cstdlib>
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        bool negative = (dividend < 0) ^ (divisor < 0);
+
+        long dvd = labs(dividend);
+        long dvs = labs(divisor);
+        int result = 0;
+
+        while (dvd >= dvs) {
+            long temp = dvs;
+            int multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            result += multiple;
+        }
+
+        return negative ? -result : result;
+    }
+};
