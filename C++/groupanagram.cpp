1d4d14eba34a556392cbcd2ffc1f3e79d70d58ed
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-05-18 23:16:56 +0800

    group anagram solution in c++ first commit

---
+++ C++/groupanagram.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+        
+        for (const string& s : strs) {
+            int count[26] = {0};
+            for (char c : s) count[c - 'a']++;
+            
+            // Build key like "1#0#2#0#..." so counts don't collide
+            string key;
+            for (int i = 0; i < 26; i++) {
+                key += to_string(count[i]);
+                key += '#';
+            }
+            
+            groups[key].push_back(s);
+        }
+        
+        vector<vector<string>> result;
+        for (auto& [_, group] : groups) {
+            result.push_back(move(group));
+        }
+        return result;
+    }
+};
