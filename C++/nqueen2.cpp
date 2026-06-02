de0efe4f6fa1700d36368575dff25ffc428394c9
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-06-02 22:59:07 +0800

    solution in c++ for nqueen2

---
+++ C++/nqueen2.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int count = 0;
+
+    void solve(int row, int n,
+               vector<int>& cols,
+               vector<int>& diag1,
+               vector<int>& diag2) {
+
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (cols[col] ||
+                diag1[row - col + n - 1] ||
+                diag2[row + col])
+                continue;
+
+            cols[col] = 1;
+            diag1[row - col + n - 1] = 1;
+            diag2[row + col] = 1;
+
+            solve(row + 1, n, cols, diag1, diag2);
+
+            cols[col] = 0;
+            diag1[row - col + n - 1] = 0;
+            diag2[row + col] = 0;
+        }
+    }
+
+    int totalNQueens(int n) {
+        vector<int> cols(n, 0);
+        vector<int> diag1(2 * n - 1, 0);
+        vector<int> diag2(2 * n - 1, 0);
+
+        solve(0, n, cols, diag1, diag2);
+
+        return count;
+    }
+};
