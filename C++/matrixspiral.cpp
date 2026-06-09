90b85e3d640a04d4387b7b1543a6f90b02b2eae3
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-06-09 23:55:06 +0800

    c++ solution

---
+++ C++/matrixspiral.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        if (matrix.empty()) return result;
+
+        int top = 0, bottom = (int)matrix.size() - 1;
+        int left = 0, right = (int)matrix[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            // go right
+            for (int i = left; i <= right; i++)
+                result.push_back(matrix[top][i]);
+            top++;
+
+            // go down
+            for (int i = top; i <= bottom; i++)
+                result.push_back(matrix[i][right]);
+            right--;
+
+            // go left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    result.push_back(matrix[bottom][i]);
+                bottom--;
+            }
+
+            // go up
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    result.push_back(matrix[i][left]);
+                left++;
+            }
+        }
+
+        return result;
+    }
+};
