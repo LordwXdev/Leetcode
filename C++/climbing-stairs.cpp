46810ef2bf6262745292f75c98f1e7ec7ebb2420
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-08-04 20:01:46 +0800

    solution in cpp for the climbing stairs problem.

---
+++ C++/climbing-stairs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int prev2 = 1, prev1 = 2, curr;
+
+        for (int i = 3; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
+
+int main() {
+    Solution sol;
+    cout << sol.climbStairs(2) << endl; // 2
+    cout << sol.climbStairs(3) << endl; // 3
+    return 0;
+}
